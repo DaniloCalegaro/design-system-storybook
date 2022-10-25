31af89aae5ff55939cc63f8fe27ec789c0daf39a
Danilo Calegaro <danilo.calegaro@hotmail.com>
2022-10-25 17:43:02 -0300

    feat: configuration storybook

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,18 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Button, ButtonProps } from '../../../react/dist'
+
+export default {
+  title: 'Button',
+  component: Button,
+  args: {
+    children: 'Enviar'
+  }
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    size: 'big'
+  }
+}
